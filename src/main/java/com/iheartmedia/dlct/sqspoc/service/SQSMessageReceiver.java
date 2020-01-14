c38e6170537530ce7e12d30485568a58a2cd5221
Lei Li <lei.li@delta.com>
2020-01-14 13:59:48 -0600

    SqsListener should use property values

--- src/main/java/com/iheartmedia/dlct/sqspoc/service/SQSMessageReceiver.java
+++ src/main/java/com/iheartmedia/dlct/sqspoc/service/SQSMessageReceiver.java
@@ -2,6 +2,7 @@ package com.iheartmedia.dlct.sqspoc.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.handler.annotation.Header;
@@ -13,9 +14,7 @@ public class SQSMessageReceiver {
 
     private static final Logger logger = LoggerFactory.getLogger(SQSMessageReceiver.class);
 
-    static private final String QUEUE_NAME = "dlct_poc.fifo";
-
-    @SqsListener(QUEUE_NAME)
+    @SqsListener(value = "${sqs.queueName}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void receiveMessage(String message, @Header("SenderId") String senderId) {
         logger.info("Received message: {}, having SenderId: {}", message, senderId);
     }
